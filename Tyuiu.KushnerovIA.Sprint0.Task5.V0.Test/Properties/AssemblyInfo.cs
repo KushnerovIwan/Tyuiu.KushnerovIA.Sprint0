62f80f69acf3f5346d986e0cba16a6708b3231b9
tyuiu.KushnerovIA@outlook.com <tyuiu.KushnerovIA@outlook.com>
2023-10-01 18:23:01 +0500

    Task5

---
+++ Tyuiu.KushnerovIA.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KushnerovIA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.KushnerovIA.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4412540d-28f6-4bcb-9cd7-be9ad5f7392a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KushnerovIA.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KushnerovIA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(12, 6));
+            Console.WriteLine("A - B = " + DataService.Substraction(12, 6));
+            Console.WriteLine("A * B = " + DataService.Multiplication(12, 6));
+            Console.WriteLine("A / B = " + DataService.Division(12, 6));
+            
+            Console.ReadKey();
+        }
+    }
+}
