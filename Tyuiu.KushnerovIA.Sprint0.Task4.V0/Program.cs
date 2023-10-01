9e291cde508eab2d12170ead984f0a60bf304c11
tyuiu.KushnerovIA@outlook.com <tyuiu.KushnerovIA@outlook.com>
2023-10-01 17:40:20 +0500

    Task4

---
+++ Tyuiu.KushnerovIA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KushnerovIA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KushnerovIA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(12, 6));
+            Console.WriteLine(DataService.Substraction(12, 6));
+            Console.WriteLine(DataService.Division(12, 6));
+            Console.WriteLine(DataService.Multiplication(12, 6));
+            Console.ReadKey();
+
+        }
+    }
+}
